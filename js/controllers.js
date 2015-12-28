5d04f3d80ddcbef184429a6ebd3787b97c6a3489
AlexanderRylov <AlexanderRylov@coherentsolutions.com>
2015-12-28 17:20:29 +0300

    test 1

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,20 @@
 'use strict';
+var myApp = angular.module('testapp',[]);
+
 
 /* Controllers */
+myApp.controller('parentCtrl1',  function($scope) {
+    //
+});
+
+myApp.controller('parentCtrl2', function($scope) {
+    //
+});
+
+myApp.controller('ChildCtrl1', function($scope) {
+    //
+});
+
+myApp.controller('ChildCtrl2', function($scope) {
+    //
+});
