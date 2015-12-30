01631981d20c3948d72711b596e6d27bbdb5bdcf
AlexanderRylov <AlexanderRylov@coherentsolutions.com>
2015-12-30 17:26:07 +0300

    task 2

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,17 @@
 'use strict';
+var myApp = angular.module('testapp',[]);
 
-/* Controllers */
+myApp.controller('tabsCtrl', ['$scope', '$http', '$sce', function ($scope, $http, $sce) {
+    $scope.lazyLoading = function (tab_id, html_source_url) {
+        if (typeof $scope.htmlfortab[tab_id] == 'undefined')
+            $http({
+                method: 'GET',
+                url: html_source_url
+            }).success(function(data){
+                $scope.htmlfortab[tab_id] = $sce.trustAsHtml(data);
+            }).error(function(){
+                $scope.htmlfortab[tab_id] = $sce.trustAsHtml('Error getting data for tab '+tab_id);
+            });
+    };
+}]);
+
--- js/directives.js
+++ js/directives.js
@@ -1,3 +1,43 @@
 'use strict';
 
 /* Directives */
+myApp.directive('myTabs', function () {
+    return {
+        restrict: 'E',
+        transclude: true,
+        scope: {},
+        controller: ['$scope', function ($scope) {
+            var panes = $scope.panes = [];
+
+            $scope.select = function (pane) {
+                angular.forEach(panes, function (pane) {
+                    pane.selected = false;
+                });
+                pane.selected = true;
+                pane.mycallBackMethod();
+            };
+
+            this.addPane = function (pane) {
+                if (panes.length === 0) {
+                    $scope.select(pane);
+                }
+                panes.push(pane);
+            };
+        }],
+        templateUrl: 'my-tabs.html'
+    };
+}).directive('myPane', function () {
+    return {
+        require: '^myTabs',
+        restrict: 'E',
+        transclude: true,
+        scope: {
+            title: '@',
+            mycallBackMethod: '&ontabactivated'
+        },
+        link: function (scope, element, attrs, tabsCtrl) {
+            tabsCtrl.addPane(scope);
+        },
+        templateUrl: 'my-pane.html'
+    };
+});
