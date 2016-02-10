721de9d9c12aacdcc72349a04220204f4b23daee
AlexanderRylov <AlexanderRylov@coherentsolutions.com>
2016-02-10 15:55:51 +0300

    task 6

--- js/directives.js
+++ js/directives.js
@@ -1,24 +1,3 @@
 'use strict';
 
 /* Directives */
-myApp.directive('setcolor', function factory(Colors) {
-    return {
-        restrict: 'E',
-        scope: {},
-        controller: ['$scope', function ($scope) {
-            $scope.generatecolor = function() {
-                Colors.getRandomColor();
-            }
-        }],
-        templateUrl: 'setcolor.html'
-    };
-}).directive('showcolor', function factory(Colors) {
-    return {
-        restrict: 'E',
-        scope: {},
-        controller: ['$scope', function ($scope) {
-            $scope.Color = Colors;
-        }],
-        templateUrl: 'showcolor.html'
-    };
-});
